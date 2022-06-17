57fb3a570bfa94b3a5cdb8097fcde339c49bfe1d
KnightLeo602 <knight_leo@aol.com>
2022-06-17 18:35:07 -0300

    tabela e outros arrays pra comparar o tempo

--- etc.java
+++ etc.java
@@ -10,6 +10,29 @@ public abstract class etc {
         return array;
     }
 
+    public static int[] invertArray(int[] array){
+        for(int i = 0; i < array.length/2; i++){
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        return array;
+    }
+
+    public static int[] deixarQuaseOrdenado(int[] array){
+        Random gerador = new Random();
+        int qntdPraEmbaralhar = (int)(array.length*0.2);
+        while(qntdPraEmbaralhar >= 0){
+            int pos1 = gerador.nextInt(array.length);
+            int pos2 = gerador.nextInt(array.length);
+            int temp = array[pos1];
+            array[pos1] = array[pos2];
+            array[pos2] = temp;
+            qntdPraEmbaralhar--;
+        }
+        return array;
+    }
+
     public static long start = 0;
     public static void comecarCronometro(){
         etc.start = System.currentTimeMillis();
@@ -18,7 +41,6 @@ public abstract class etc {
         return (System.currentTimeMillis() - start) / 100F;
     }
 
-
     public static void printArray(int[] array){
         System.out.print("[");
         for(int i = 0; i < array.length; i++){
@@ -26,5 +48,28 @@ public abstract class etc {
             if(i + 1 != array.length) System.out.print(", ");
         }
         System.out.print("]");
+    }
+
+    public static String verTamMax_table(Object texto, int max){
+        String tex = texto.toString();
+        String tex2;
+        boolean rev = false;
+        while(tex.length() < max){
+            if(rev == false){
+                rev = true;
+                tex = tex + " ";
+            }else{
+                rev = false;
+                tex = " " + tex;
+            }
+        }
+        while(tex.length() > max){
+            tex2 = "";
+            for(int i = 0; i < tex.length() - 1; i++){
+                tex2 += tex.charAt(i); 
+            }
+            tex = tex2;
+        }
+        return tex;
     }
 }
--- sorts.java
+++ sorts.java
@@ -1,11 +1,58 @@
 public class sorts {
     public static void main(String[] args) throws Exception {
-        int[] array = etc.criarArray(10000, 10000);
-        System.out.println("QuickSort comecou! ");
+
+        int tamanhoDoConjunto = 10_000;
+
+        int[] arrayDesordenado = etc.criarArray(tamanhoDoConjunto, 10000);
+        int[] arrayOrdenado = quicksort(arrayDesordenado.clone(), 0, arrayDesordenado.length-1);
+        int[] arrayQuaseOrdenado = etc.deixarQuaseOrdenado(arrayOrdenado.clone());
+        int[] arrayDescrecente = etc.invertArray(arrayOrdenado.clone());
+
+        
+        // QUICK SORT
+        float[] temposQuickSort = new float[3];
+        System.out.print("QuickSort em array Desordenado: ");
         etc.comecarCronometro();
-        quicksort(array, 0, array.length-1);
-        System.out.println("QuickSort Terminou! Tempo: " + etc.pararCronometro() + " s");
+        quicksort(arrayDesordenado, 0, arrayDesordenado.length-1);
+        temposQuickSort[0] = etc.pararCronometro();
+        System.out.println("Tempo: " + temposQuickSort[0] + " s");
+
+        System.out.print("QuickSort em array Quase ordenado: ");
+        etc.comecarCronometro();
+        quicksort(arrayQuaseOrdenado, 0, arrayQuaseOrdenado.length-1);
+        temposQuickSort[1] = etc.pararCronometro();
+        System.out.println("Tempo: " + temposQuickSort[1] + " s");
+
+        System.out.print("QuickSort em array Decrescente: ");
+        etc.comecarCronometro();
+        quicksort(arrayDescrecente, 0, arrayDescrecente.length-1);
+        temposQuickSort[2] =  etc.pararCronometro();
+        System.out.println("Tempo: " + temposQuickSort[2] + " s");
         //etc.printArray(quicksort(array, 0, array.length-1));
+
+        // SHELL SORT 
+        float[] temposShellSort = new float[3];
+
+
+        //HEAP SORT
+        float[] temposHeapSort = new float[3];
+        
+        // eu n to com vontade de fazer uma tabela dinamica, entao vai ficar assim
+        System.out.println("");
+        System.out.println("_".repeat(65));
+        System.out.print("|");     System.out.print(etc.verTamMax_table("Tamanho do conjunto: " + tamanhoDoConjunto, 63));     System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(63));       System.out.println("|");
+        System.out.print("|");     System.out.print(etc.verTamMax_table(" ", 15));      System.out.print("|");     System.out.print(etc.verTamMax_table("Tempo obtidos pelos algoritmos", 47));      System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(15));       System.out.print("|");     System.out.print("_".repeat(47));       System.out.println("|");
+        System.out.print("|");     System.out.print(etc.verTamMax_table("Algoritmo", 15));     System.out.print("|");     System.out.print(etc.verTamMax_table("QuaseOrdenado", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table("Desordenado", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table("Decrescente", 15));     System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.println("|");
+        System.out.print("|");     System.out.print(etc.verTamMax_table("QuickSort", 15));     System.out.print("|");     System.out.print(etc.verTamMax_table(temposQuickSort[0] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposQuickSort[1] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposQuickSort[2] + " s", 15));     System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.println("|");
+        System.out.print("|");     System.out.print(etc.verTamMax_table("ShellSort", 15));     System.out.print("|");     System.out.print(etc.verTamMax_table(temposShellSort[0] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposShellSort[1] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposShellSort[2] + " s", 15));     System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.println("|");
+        System.out.print("|");     System.out.print(etc.verTamMax_table("HeapSort", 15));     System.out.print("|");     System.out.print(etc.verTamMax_table(temposHeapSort[0] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposHeapSort[1] + " s", 15));     System.out.print("|");      System.out.print(etc.verTamMax_table(temposHeapSort[2] + " s", 15));     System.out.println("|");
+        System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");     System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.print("|");      System.out.print("_".repeat(15));     System.out.println("|");
+
     }
 
     public static int[] quicksort(int[] A, int low, int high){
